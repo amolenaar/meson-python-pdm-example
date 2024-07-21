fd215713698baf92e5b96e7f1b68f8b76bc39896
Arjan Molenaar <gaphor@gmail.com>
2024-07-21 22:02:49 +0200

    Remove pkgutil config

--- meson_python_pdm_example/__init__.py
+++ meson_python_pdm_example/__init__.py
@@ -1,7 +1,3 @@
 # SPDX-FileCopyrightText: 2024 Arjan Molenaar
 #
 # SPDX-License-Identifier: Apache-2.0
-
-# This will make sure we use 
-from pkgutil import extend_path
-__path__ = extend_path(__path__, __name__)
